0dca8765f5a1c39f4365ca8b89b3f929d685ee6c
akashnag <akashnag1800@gmail.com>
2025-08-17 14:34:08 +0530

    Initial commit for sentiment analysis app

--- bhagavad_gita_sentiment_analysis.py
+++ bhagavad_gita_sentiment_analysis.py
@@ -1,14 +1,6 @@
 import streamlit as st
 import pandas as pd
-import nltk
 
-# Ensure nltk 'punkt' tokenizer is downloaded (with check to avoid errors on Streamlit Cloud)
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt')
-
-# Load your Dataset.xlsx containing sentiment lexicon
 df = pd.read_excel('Dataset.xlsx')
 
 def build_sentiment_dict(df, word_col='Name of Word', pos_col='Positive', neg_col='Negative'):
@@ -27,20 +19,17 @@ def build_sentiment_dict(df, word_col='Name of Word', pos_col='Positive', neg_co
 sentiment_dict = build_sentiment_dict(df)
 
 def compute_sentiment_all_words(text):
-    words = nltk.word_tokenize(text.lower())
+    import re
+    words = re.findall(r'\b\w+\b', text.lower())
     results = []
     pos_total, neg_total = 0, 0
     for word in words:
-        if word.isalpha():
-            pos, neg = sentiment_dict.get(word, (0, 0))
-            if not isinstance(pos, (int, float)): pos = 0
-            if not isinstance(neg, (int, float)): neg = 0
-            results.append((word, pos, neg))
-            pos_total += pos
-            neg_total += neg
-        else:
-            # Include punctuation or non-alpha with 0 scores if needed
-            results.append((word, 0, 0))
+        pos, neg = sentiment_dict.get(word, (0, 0))
+        if not isinstance(pos, (int, float)): pos = 0
+        if not isinstance(neg, (int, float)): neg = 0
+        results.append((word, pos, neg))
+        pos_total += pos
+        neg_total += neg
     if pos_total > neg_total:
         overall_sentiment = "Positive"
     elif neg_total > pos_total:
@@ -63,7 +52,6 @@ if user_text:
     df_words = pd.DataFrame(word_scores, columns=['Word', 'Positive', 'Negative'])
     st.table(df_words)
 
-    # Additional: Show summary message
     if overall_sentiment == "Positive":
         st.success("The overall sentiment of the verse is POSITIVE.")
     elif overall_sentiment == "Negative":
